633c3524c0607692674a162c065a22846692883d
mmann30 <michael.l.mann13@gmail.com>
2017-05-21 09:12:50 -0400

    base game functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,66 @@
 // Crystal Collector JS
 
-// Initiating target and player score values
-var targetScore = 0;
-var playerScore = 0;
+// Variables
+var targetScore;
+var playerScore;
+var blueCrystal;
+var greenCrystal;
+var redCrystal;
+var yellowCrystal;
 
-// function to create a random number to incriment by.
-function getRanNum(){
-	var ranNum = Math.floor(Math.random() * 50) + 1;
+// Function to create a random Target number
+function getTargetNum(){
+	var targetNum = Math.floor(Math.random() * 100) + 50;
+	return targetNum;
+}
+// Generates random score values when for each button
+function getScoreNum(){
+	var scoreNum = Math.floor(Math.random() * 20) + 1;
+	return scoreNum;
 }
 
-
-$(".blue").on("click", function(){
-	playerScore = getRanNum();
+// Initializing and resets scores
+function reset(){
+	targetScore = getTargetNum();
+	playerScore = 0;
+	blueCrystal = getScoreNum();
+	greenCrystal = getScoreNum();
+	redCrystal = getScoreNum();
+	yellowCrystal = 1;
+	// Display iniltial target and player scores
+	$(".target-score").html(targetScore);
 	$(".player-score").html(playerScore);
-});
+}
+
+reset();
+
+	// Incriments score by crystal value
+	$(".blue").on("click", function(){
+		playerScore += blueCrystal;
+		$(".player-score").html(playerScore);
+		
+		if(playerScore === targetScore){
+			alert("Congratulations! You Win!");
+			reset();
+		} else if (playerScore > targetScore){
+			alert("You lose");
+			reset();
+		}
+	});
+
+	$(".green").on("click", function(){
+		playerScore += greenCrystal;
+		$(".player-score").html(playerScore);
+	});
+
+	$(".red").on("click", function(){
+		playerScore += redCrystal;
+		$(".player-score").html(playerScore);
+	});
+
+	$(".yellow").on("click", function(){
+		playerScore += yellowCrystal;
+		$(".player-score").html(playerScore);
+	});
+
+
